289b82b7579fa68c63d2d0f45494cc204c870125
dermio <thisdjay@gmail.com>
2021-08-18 23:06:38 -0700

    Add options for CORS in server

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
-const io = require("socket.io")(3000);
+const options = {
+  cors: {
+    origin: "*"
+  }
+};
+
+// io is the server instance
+const io = require("socket.io")(3000, options);
 
 io.on("connection", socket => {
-  console.log("chat-message", "Hello World");
+  console.log("new user");
+  socket.emit("chat-message", "Hello World");
 });

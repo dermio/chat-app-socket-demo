d7687f6f3228e6d0a6f73a9f8f172141424fe798
dermio <thisdjay@gmail.com>
2021-08-18 23:10:19 -0700

    Add scripts for socket.io in index.html

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // All the client side code goes here
 
-// The location of the socket is hosted at localhost:3000.
+// The location of the server that's hosting the socket at localhost:3000.
 const socket = io("http://localhost:3000");
 
 socket.on("chat-message", data => {

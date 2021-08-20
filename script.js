12d0b58cccc0bfeaee9fbcd1662c4664f0583134
dermio <thisdjay@gmail.com>
2021-08-19 20:31:00 -0700

    Text message input is emitted on form submission
    Add event listener for text message input form submission.
    The text message input is emitted via the socket.

--- script.js
+++ script.js
@@ -2,7 +2,22 @@
 
 // The location of the server that's hosting the socket at localhost:3000.
 const socket = io("http://localhost:3000");
+const messageForm = document.getElementById("send-container");
+const messageInput = document.getElementById("message-input");
 
 socket.on("chat-message", data => {
   console.log(data);
+});
+
+messageForm.addEventListener("submit", event => {
+  event.preventDefault();
+
+  // Get text message from input.
+  let message = messageInput.value;
+
+  // Send text message value from client to server via the socket.
+  socket.emit("send-chat-message", message);
+
+  // Clear out any text in the input field.
+  messageInput.value = "";
 });

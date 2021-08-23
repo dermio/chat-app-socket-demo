f686fd5a421f7452419aefd83b6b92e02eff3359
dermio <thisdjay@gmail.com>
2021-08-22 22:43:43 -0700

    The server saves new users and broadcasts a new user joined
    The socket server listens for any new users.
    The server then adds the new user to the users object.
    Finally the server tells the other users a new user joined.

--- server.js
+++ server.js
@@ -7,7 +7,19 @@ const options = {
 // io is the server instance
 const io = require("socket.io")(3000, options);
 
+const users = {}; // Save chat users in an object.
+
 io.on("connection", socket => {
+  // The socket listens for any new users.
+  socket.on("new-user", name => {
+    /* Save the new user to the users object with the unique socket id
+    as the user key. */
+    users[socket.id] = name;
+
+    // Broadcast to the other users a new user has joined.
+    socket.broadcast.emit("user-connected", name);
+  });
+
   // The socket.on method listens for the event type, then runs the callback.
   socket.on("send-chat-message", message => {
     /* The socket.broadcast.emit server method sends events

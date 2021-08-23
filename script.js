647d59c26d58a7320a58d24c46b79b66e8ad97e7
dermio <thisdjay@gmail.com>
2021-08-22 19:13:34 -0700

    Append messages from other users to the display

--- script.js
+++ script.js
@@ -2,11 +2,16 @@
 
 // The location of the server that's hosting the socket at localhost:3000.
 const socket = io("http://localhost:3000");
+const messageContainer = document.getElementById("message-container");
 const messageForm = document.getElementById("send-container");
 const messageInput = document.getElementById("message-input");
 
 socket.on("chat-message", data => {
-  console.log(data);
+  /* When the client hears the `chat-message` event from the server,
+  append the message sent by the other client. The message was sent
+  from the other client to the server, and from the server to the client
+  via the socket. */
+  appendMessage(data);
 });
 
 messageForm.addEventListener("submit", event => {
@@ -21,3 +26,14 @@ messageForm.addEventListener("submit", event => {
   // Clear out any text in the input field.
   messageInput.value = "";
 });
+
+function appendMessage(message) {
+  // Create div element to contain message.
+  let messageElement = document.createElement("div");
+
+  // Add text message to message element.
+  messageElement.innerText = message;
+
+  // Append message element to message container.
+  messageContainer.append(messageElement);
+}
